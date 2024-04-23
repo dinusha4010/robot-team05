9a14edca4fc600f84e779634338b051c0c825883
KaluarachchiKCD <dinusha4010@gmail.com>
2024-04-23 19:11:40 +0300

    Robot java class created

---
+++ legoRobot/src/data/Robot.java
@@ -0,0 +1,53 @@
+package data;
+
+public class Robot {
+
+	private static int speed;
+	private static int direction;
+	private static int run;
+
+	public static int getSpeed() {
+		return speed;
+	}
+
+	public static void setSpeed(int speed) {
+		Robot.speed = speed;
+	}
+
+	public static void setSpeed(String speed) {
+		try {
+			Robot.speed = Integer.parseInt(speed);
+		} catch (Exception e) {
+
+		}
+
+	}
+
+	public static int getDirection() {
+		return direction;
+	}
+
+	public static void setDirection(int direction) {
+		Robot.direction = direction;
+	}
+
+	public static void setDirection(String direction) {
+		try {
+			Robot.direction = Integer.parseInt(direction);
+		}
+
+		catch (Exception e) {
+
+		}
+
+	}
+
+	public static int getRun() {
+		return run;
+	}
+
+	public static void setRun(int run) {
+		Robot.run = run;
+	}
+
+}
