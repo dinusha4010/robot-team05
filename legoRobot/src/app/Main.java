1f664fd301b881275f894f3ca1071a50b01e6c3b
KaluarachchiKCD <dinusha4010@gmail.com>
2024-04-29 07:50:11 +0300

    All threads added to main programme

--- legoRobot/src/app/Main.java
+++ legoRobot/src/app/Main.java
@@ -2,24 +2,38 @@ package app;
 
 import threads.LightSensorThread;
 import threads.ReadLineFollowerData;
+import threads.ReadObstacale;
 import threads.ReadRunStatusData;
 import threads.RunLego;
+import threads.UltrasonicSensor;
+import threads.UpdateCurrentSpeed;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-		 Thread thread = new Thread(new ReadLineFollowerData());
-	        thread.start();
-	        
 
-			Thread thread2 = new Thread(new RunLego());
-		    thread2.start();
-		    
-		    Thread thread3 = new Thread(new ReadRunStatusData());
-	        thread3.start();
-	        Thread thread4 = new Thread(new LightSensorThread());
-	        thread4.start();
+		Thread thread = new Thread(new LightSensorThread());
+
+		Thread thread1 = new Thread(new ReadLineFollowerData());
+
+		Thread thread2 = new Thread(new ReadObstacale());
+
+		Thread thread3 = new Thread(new ReadRunStatusData());
+		
+		Thread thread4 = new Thread(new RunLego());
+		
+		Thread thread5 = new Thread(new UltrasonicSensor());
+		
+		
+		
+		
+
+		thread.start();
+		thread1.start();
+		thread2.start();
+		thread3.start();
+		thread4.start();
+		thread5.start();
 
 	}
 
